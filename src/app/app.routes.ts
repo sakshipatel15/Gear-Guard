049b276972504a0f9f05f792f2eeb751fd4f917d
snehagodhani12 <snehagodhani90@gmail.com>
2025-12-27 13:21:00 +0530

    add all changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { CommandDashboardComponent } from './dashboard/command-dashboard/command-dashboard.component';
 import { StatCardComponent } from './shared/stat-card/stat-card.component';
+import { EquipmentDetailComponent } from './equipment/equipment-detail/equipment-detail.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
         path: 'statcard',
         component: StatCardComponent
+    },
+    {
+        path: 'detail',
+        component: EquipmentDetailComponent
     },
     {
         path: 'navbar', // Keeping for specific testing if needed, but usually redundant now
